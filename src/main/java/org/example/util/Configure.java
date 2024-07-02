186ab101dcc1a70fdafe409e50456a8b07cc43e0
seenpc15 <sktds92@seedn.co.kr>
2024-07-02 10:45:07 +0900

    properties 환경별 읽어오는 path 변경

--- src/main/java/org/example/util/Configure.java
+++ src/main/java/org/example/util/Configure.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Properties;
 
 public class Configure {
@@ -25,6 +26,29 @@ public class Configure {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void initProperties(){
+        String configFilePath = this.getConfigFilePath();
+        this.initProperties(configFilePath);
+    }
+
+    private String getConfigFilePath() {
+        String env = System.getenv("ENV");
+        String fileName = "";
+        if(env.equals("prod") || env.equals("PROD") || env.equals("live")){
+            fileName = "config-prod.properties";
+        }else if(env.equals("dev") || env.equals("DEV") || env.equals("develop")){
+            fileName = "config-dev.properties";
+        }else{
+            fileName = "config.properties";
+        }
+        return this.makeConfigFilePath(fileName);
+    }
+
+    private String makeConfigFilePath(String fileName) {
+        URL resource = Configure.class.getClassLoader().getResource(fileName);
+        return resource.getPath();
     }
 
     public static synchronized  Configure getInstance(){
