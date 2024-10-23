741d735b69fc5f75be88b5cc16b1198d51070483
seenpc15 <sktds92@seedn.co.kr>
2024-10-23 10:32:43 +0900

    error message producer 생성

---
+++ src/main/java/org/example/util/ErrorProducer.java
@@ -0,0 +1,40 @@
+package org.example.util;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.example.common.AbsSdnObject;
+import java.util.Properties;
+import java.util.List;
+
+public class ErrorProducer extends AbsSdnObject {
+    private static ErrorProducer instance;
+    private Properties properties;
+    private KafkaProducer<String, String> kafkaProducer;
+
+    private ErrorProducer(Properties properties){
+        this.properties = properties;
+        this.kafkaProducer = new KafkaProducer<>(properties);
+    }
+    public static synchronized ErrorProducer getInstance(Properties properties){
+        if(instance == null){
+            instance = new ErrorProducer(properties);
+        }
+        return instance;
+    }
+
+    public void send(List<ProducerRecord<String,String>> messageList){
+        for(ProducerRecord producerRecord: messageList){
+            this.kafkaProducer.send(producerRecord);
+        }
+        this.kafkaProducer.flush();
+        try{
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            logger.error(e);
+        }
+    }
+
+    public void send(ProducerRecord<String,String> message){
+        this.kafkaProducer.send(message);
+    }
+}
